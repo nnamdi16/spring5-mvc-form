72e35198989ff82e2cfaa079b5e00bb58876850b
Nnamdi <nwabuokeinnamdi@gmail.com>
2019-09-18 16:46:20 +0100

    Read data from the form and pass it to a message and display it

--- src/main/java/com/nnamdi/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/nnamdi/springdemo/mvc/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.nnamdi.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -15,6 +18,26 @@ public class HelloWorldController {
     //Controller to process the form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    //new Controller method to read form data
+    //Add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        //Read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        //Convert the data to all caps
+        theName = theName.toUpperCase();
+
+        //Create the message
+        String result = "Yo! " + theName;
+
+        //Add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
