d110c8a3478ccf6cfb6ae6dc33b0e64b6aa1d2f5
Nnamdi <nwabuokeinnamdi@gmail.com>
2019-09-22 21:11:16 +0100

    Pass the wrapper class Integer to convert primitive integer class to Object Integer

--- src/main/java/com/nnamdi/springdemo/mvc/Customer.java
+++ src/main/java/com/nnamdi/springdemo/mvc/Customer.java
@@ -1,8 +1,7 @@
 package com.nnamdi.springdemo.mvc;
 
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 ;
 
@@ -16,6 +15,14 @@ public class Customer {
     @NotEmpty(message = "Please enter last name")
     @Size(min = 5, max = 100, message = "last name invalid")
     private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "Must be greater or equal to zero")
+    @Max(value = 10, message = "Must be less than or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 Chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -31,5 +38,21 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
